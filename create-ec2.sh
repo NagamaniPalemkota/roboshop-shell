2dfccfa5f00382c1c0094f5bcb5728af259fefd8
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-25 13:52:35 +0530

    committing shell for roboshop

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,6 +3,22 @@
 instances=("MongoDB" "catalogue" "Redis" "web" "MySQL" "User" "cart" "shipping" "RabbitMQ" "Payment" )
 hosted_zone_id=Z09196511SQGIFEK0HWMC
 domain_name="muvva.online"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 197
+    else
+        echo -e "$2 is $G success $N"
+    fi
+}
+
 for name in ${instances[@]};
 do 
     if [ $name == "MySQL" ] || [ $name == "shipping" ]
@@ -14,16 +30,20 @@ do
     echo "Creating instance: $name with instance type: $instance_type"
     instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32  --instance-type $instance_type  --security-group-ids 
 	sg-0068acc7a96fbb265  --subnet-id subnet-0717dc25807226641 --query "Reservations[].Instances[].InstanceId" --output text)
+    validate $? "Creating instance $name"
 
     aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+    validate $? "Creating tags for $name"
 
     if [ $name == "web" ]
     then
         aws ec2 wait instance-running --instance-ids $instance_id
         public_ip=$(ec2 describe-instances --filters "Name=instance_id,Values=$instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
+         validate $? "Fetching $public_ip for $name"
         ip=$public_ip
     else
         private_ip=$(ec2 describe-instances --filters "Name=instance_id,Values=$instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+         validate $? "Fetching $private_ip for $name"
         ip=$private_ip
     fi
     aws route53 change-resource-record-sets --hosted-zone-id  $hosted_zone_id --change-batch 
@@ -40,4 +60,5 @@ do
         } 
         } ]
          }'
+        validate $? "creating/updating route53 record for $name:"
 done
