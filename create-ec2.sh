db67f49835b4559da33e5be41ded5c188e4f0d58
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-25 11:44:54 +0530

    committing shell for roboshop

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-instances = ("MongoDB" "catalogue" "Redis" "web" "MySQL" "User" "cart" "shipping" "RabbitMQ" "Payment" )
+instances=("MongoDB" "catalogue" "Redis" "web" "MySQL" "User" "cart" "shipping" "RabbitMQ" "Payment" )
 
 for name in ${instances[@]}
 do 
     if (name == "MySQL") || (name == "shipping")
     then
-        instance_type = "t3.medium"
+        instance_type="t3.medium"
     else
-        instance_type = "t3.micro"
+        instance_type="t3.micro"
     fi
     echo ("Creating instance: $name with instance type: $instance_type")
 done
