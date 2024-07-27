7f23598cf713e7556870fb5b057e3c47b02cc4c6
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-27 15:45:16 +0530

    committing shell for roboshop

---
+++ shipping.sh
@@ -0,0 +1,82 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo "$0" | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 197
+    else
+        echo -e "$2 is $G success $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
+else
+    echo "You are super user"
+fi
+
+dnf install maven -y &>> $LOGFILE
+VALIDATE $? "Installing Maven"
+
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "Adding roboshop user"
+else
+    echo -e "User roboshop alreay exists $Y SKIPPING $N"
+fi
+
+rm -rf /app &>> $LOGFILE 
+VALIDATE $? "removing existing app directory"
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "Creating directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+VALIDATE $? "downloading shipping code"
+
+cd /app &>> $LOGFILE
+VALIDATE $? "changing to app directory"
+
+mvn clean package &>> $LOGFILE
+VALIDATE $? "cleaning maven package"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+VALIDATE $? "renaming jar file"
+
+mv /home/ec2-user/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+VALIDATE $? "copying jar file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Daemon reloading the service"
+
+systemctl enable shipping &>> $LOGFILE
+VALIDATE $? "enabling the shipping service"
+
+systemctl start shipping &>> $LOGFILE
+VALIDATE $? "starting the shipping service"
+
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing mysql"
+
+
+mysql -h mysql.muvva.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+VALIDATE $? "loading shipping schema into mysql"
+
+systemctl restart shipping &>> $LOGFILE
+VALIDATE $? "Restarting the shipping service"
