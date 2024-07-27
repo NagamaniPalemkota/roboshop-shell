cb520b13f5ef2621ff283ef556b434f2194741ee
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-07-27 15:08:52 +0530

    committing shell for roboshop

---
+++ cart.sh
@@ -0,0 +1,79 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo "$0" | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGO_HOST="mongodb.muvva.online"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 197
+    else
+        echo -e "$2 is $G success $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
+else
+    echo "You are super user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "disabling mongodb"
+
+dnf module enable nodejs:20 -y &>> $LOGFILE
+VALIDATE $? "eanbling nodejs"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing nodejs"
+
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "Adding roboshop user"
+else
+    echo -e "User roboshop alreay exists $Y SKIPPING $N"
+fi
+
+rm -rf /app &>> $LOGFILE 
+VALIDATE $? "removing existing app directory"
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "Creating directory"
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>> $LOGFILE
+VALIDATE $? "downloading cart code"
+
+cd /app &>> $LOGFILE
+VALIDATE $? "changing to app directory"
+
+unzip /tmp/cart.zip &>> $LOGFILE
+VALIDATE $? "unzipping the code"
+
+npm install &>> $LOGFILE
+VALIDATE $? "installing dependencies"
+
+cp /home/ec2-user/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
+VALIDATE $? "copying cart service file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "Daemon reloading the service"
+
+systemctl enable cart &>> $LOGFILE
+VALIDATE $? "enabling the cart service"
+
+systemctl start cart &>> $LOGFILE
+VALIDATE $? "starting the cart service"
+
